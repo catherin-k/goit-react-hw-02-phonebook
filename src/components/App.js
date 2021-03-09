ff1892b51d75cce8461c0e112718dc3eadebe07a
Katarina Kyrylenko <katarina.kyrylenko@gmail.com>
2021-03-09 12:58:22 +0200

    add alert for the same contact

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,16 @@ import "./App.css";
 
 import ContactForm from "./contactForm/ContactForm";
 import ContactList from "./contactList/contactList";
+import Filter from "./filter/Filter";
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
   };
 
   addContact = (name, number) => {
@@ -16,9 +23,44 @@ class App extends Component {
       name,
       number,
     };
-    this.setState(({ contacts }) => ({
-      contacts: [contact, ...contacts],
+
+    const nameArr = this.state.contacts.reduce(
+      (acc, contact) => [...acc, contact.name.toLowerCase()],
+      []
+    );
+
+    if (nameArr.includes(name.toLowerCase())) {
+      console.log("Alert");
+      alert(`${name} is already in contacts`);
+      return;
+    } else {
+      this.setState(({ contacts }) => ({
+        contacts: [contact, ...contacts],
+      }));
+    }
+
+    console.log(nameArr);
+    console.log(name.toLowerCase());
+  };
+
+  deleteContact = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter(
+        (contact) => contact.id !== contactId
+      ),
     }));
+  };
+
+  changeFilter = (e) => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
   };
 
   // handleSubmit = (e) => {
@@ -33,7 +75,11 @@ class App extends Component {
         <h1>Phonebook</h1>
         <ContactForm onSubmit={this.addContact} />
         <h2>contacts</h2>
-        <ContactList contacts={this.state.contacts} />
+        <Filter value={this.state.filter} onChange={this.changeFilter} />
+        <ContactList
+          contacts={this.getVisibleContacts()}
+          onDeleteContact={this.deleteContact}
+        />
       </div>
     );
   }
--- src/components/contactList/contactList.js
+++ src/components/contactList/contactList.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const ContactList = ({ contacts }) => (
+const ContactList = ({ contacts, onDeleteContact }) => (
   <ul>
     {contacts.map(({ name, number, id }) => (
       <li key={id}>
         <p>
           {name}: {number}
         </p>
+        <button onClick={() => onDeleteContact(id)}>Delete</button>
       </li>
     ))}
   </ul>
